8c980ee17a05b3ecebcbf74e8730e35d07ed24db
leinefran <penny.leine@gmail.com>
2018-09-25 20:43:50 +0000

    Added: error handling

--- main.c
+++ main.c
@@ -9,5 +9,18 @@
  */
 int main(int argc, char *argv[])
 {
+	int fd;
 
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
