94e19164a52c10ea028f8d85cf9a29b30ca7cb1b
Jinji Zhang <362@holbertonschool.com>
2018-09-25 21:04:16 +0000

    Add: push element on stack function

---
+++ push.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * push - pushes an element to the stack
+ * @stack: pointer to address of the stack
+ * @line_number: number to be assigned to stack element
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new;
+
+	if (!stack)
+		return (NULL);
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = line_number;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head)
+		(*head)->prev = new;
+	*head = new;
+}
