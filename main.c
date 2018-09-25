ff8e936e605448fa3c5e0cc89772148e77fe3da9
Jinji Zhang <362@holbertonschool.com>
2018-09-25 20:34:01 +0000

    Add main file, include statements in header

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * main - interpreter for Monty ByteCodes files
+ * @argc: number of command line arguments
+ * @argv: argument vector
+ *
+ * Return: EXIT_SUCCESS upon success, else, EXIT_FAILURE
+ */
+int main(int argc, char *argv[])
+{
+
+}
--- monty.h
+++ monty.h
@@ -1,6 +1,9 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
