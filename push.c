9ac76d2c821f61544dc26ed9a6d41f270a13232e
Jinji Zhang <362@holbertonschool.com>
2018-09-26 23:22:24 +0000

    Fix: can push value 0 now

--- push.c
+++ push.c
@@ -8,6 +8,7 @@
 void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new;
+	int status;
 
 	if (!stack)
 		return;
@@ -17,16 +18,19 @@ void push(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-
-	new->n = atoi(get_value_at_node_index(line_number));
-	if (new->n == 0)
+	status = isnumber(get_value_at_node_index(line_number));
+	if (status == -1)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
+                exit(EXIT_FAILURE);
 	}
+	else
+	{
+		new->n = atoi(get_value_at_node_index(line_number));
+	}
+
 	new->next = *stack;    /* element at the given line number */
 	new->prev = NULL;
-
 	if (*stack == NULL)
 	{
 		*stack = new;
