b6a847a9c0b37329ac713465ac93aa00093f7c48
leinefran <penny.leine@gmail.com>
2018-09-26 18:01:59 +0000

    function to return appropriate function pointer

---
+++ get_instruc_func.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * get_instruc_func - a function that reads command string and selects
+ * the correct function to perform
+ * @s: command string
+ * @status: pointer to integer that will be set to 0 on success
+ * -1 if command not found
+ *
+ * Return: a pointer to the corresponding function
+ */
+void (*get_instruc_func(char *s, int *status))
+(stack_t **stack, unsigned int line_number)
+{
+	instruction_t ops[] = {
+		{"push", push},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].opcode != NULL)
+	{
+		if (strcmp(s, ops[i].opcode) == 0)
+		{
+			printf("found it\n");
+			*status = 0;
+			return (ops[i].f);
+		}
+		i++;
+	}
+	*status = -1;
+	exit(-1);
+}
