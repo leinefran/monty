cf7acb6a40d027d77126f816101a377f81fa9164
Jinji Zhang <362@holbertonschool.com>
2018-09-27 22:38:22 +0000

    Bug fix for add function

--- add.c
+++ add.c
@@ -7,26 +7,26 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	int sum;
+	int sum, count = 0;
 	stack_t *tmp;
 
+	tmp = *stack;
 	while (tmp != NULL)
 	{
 		count++;
 		tmp = tmp->next;
 	}
-	if (stack && count < 2)
+	if (count < 2)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n",
 			line_number);
 		exit(EXIT_FAILURE);
 	}
-	if (stack && count >= 2)
+	if (*stack && count >= 2)
 	{
 		tmp = (*stack)->next;
 		sum = (*stack)->n + tmp->n;
 		tmp->n = sum;
-		free(*stack);
-		stack = tmp;
+		pop(stack, line_number);
 	}
 }
